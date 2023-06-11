5769f39e04ae786a4af25c37df398dcf2aaefd81
Robby Lee <55405396+leerobby@users.noreply.github.com>
2023-06-12 02:46:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -152,12 +152,12 @@ def match():
         match_html_content += f'<p id="sport_type"><img src="{{{{url_for("static", filename="img/sport.png") }}}}" alt="Sport Icon">{row[3]}</p>'
         match_html_content += f'<p id="gender"><img src="{{{{ url_for("static", filename = "img/gender-fluid.png")}}}}" alt="Sport Icon">{row[4]}</p>'
         match_html_content += f'<p id="location"><img src="{{{{ url_for("static", filename = "img/location.png")}}}}" alt="Location Icon">{match_loc}</p>'
-        match_html_content += f'<p id="price"><img src="{{{{ url_for("static", filename = "img/price-tag.png")}}}}" alt="Price Icon">\u20a9{row[6]}</p>'
+        match_html_content += f'<p id="price"><img src="{{{{ url_for("static", filename = "img/price-tag.png")}}}}" alt="Price Icon">won{row[6]}</p>'
         match_html_content += f'<p id="player_slot">Slots: {row[7]}/{row[8]}</p><hr class="dashed"><h3>Host</h3>'
         match_html_content += f'<p id="host_name">{row[9]}</p><form action="#"><input type="button" value="Join Match" class="custom-button" id="button{count}"></form></div>'
         match_html_content += f'<script>'
         match_html_content += f'$(document).ready(function() {{'
-        match_html_content += f'$("button{count}").click(function() {{'
+        match_html_content += f'$("#button{count}").click(function() {{'
         match_html_content += f'$.ajax({{'
         match_html_content += f'url: "/join",'
         match_html_content += f'type: "POST",'
@@ -176,7 +176,6 @@ def match():
         count += 1
 
     match_file = open('templates/textFiles/match3.txt', 'r')
-    match_html_content += f'<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>'
     match_html_content += match_file.read()
 
     #output html file
